3e6722c9dc5508a53deb3bdc68914a4c286f00de
Gijs van Dam <gvandam@gmail.com>
2020-04-30 22:20:30 +0800

    Add create route with create dialog

---
+++ layouts/centered.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-layout align-center justify-center row fill-height>
+          <nuxt />
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2020 
+      </span>
+    </v-footer>
+  </v-app>
+</template>
---
+++ pages/create.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar
+      color="primary"
+      dark
+      flat
+    >
+      <v-toolbar-title>Create your own website</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="reponame"
+          label="Name"
+          name="name"
+          prepend-icon="create"
+          type="text"
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary">
+        Create
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      reponame: this.$route.query.reponame
+    }
+  },
+  layout: 'centered',
+  auth: false
+}
+</script>
