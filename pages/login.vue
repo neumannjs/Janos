527f4525ea821019f0ea9850ed2b309a86ec4668
Gijs van Dam <gvandam@gmail.com>
2020-04-30 22:20:56 +0800

    WIP Login handling different oAuth flows

--- pages/login.vue
+++ pages/login.vue
@@ -4,10 +4,33 @@
 
 <script>
 export default {
+  data() {
+    return {
+      reponame: this.$route.query.reponame
+    }
+  },
+  layout: 'centered',
   created() {
-    // console.log(this.$auth.strategies.githubProxy.options.access_token_endpoint)
-    // http://localhost:7071/api/handler/?reponame=test
-    this.$auth.loginWith('githubProxy')
+    if (this.$route.query.reponame && this.$route.query.reponame.length > 0) {
+      this.$auth.strategies.githubProxy.options.access_token_endpoint +=
+        '?reponame=' + this.$route.query.reponame
+    }
+    this.$auth.loginWith('githubProxy', {
+      // TODO: This state should include a random nonce of sorts
+      state: 'http://localhost:3000/callback/'
+    })
   }
+}
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      reponame: this.$route.query.reponame
+    }
+  },
+  layout: 'centered',
+  auth: false
 }
 </script>
