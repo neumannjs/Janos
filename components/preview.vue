f57f8ac0f287fc4530c6a60f7726135216cb5e08
Gijs van Dam <gvandam@gmail.com>
2020-05-14 13:31:05 +0800

    Fullscreen preview switch

--- components/preview.vue
+++ components/preview.vue
@@ -1,5 +1,44 @@
 <template>
-  <iframe :srcDoc="srcDoc" class="previewIFrame" />
+  <v-card height="100%" width="100%" flat tile>
+    <v-hover>
+      <v-btn
+        slot-scope="{ hover }"
+        absolute
+        dark
+        fab
+        bottom
+        right
+        small
+        class="btnOffset"
+        :style="hover ? {opacity: '1'} : {opacity: '0.3'}"
+        @click="fullscreen = true"
+      >
+        <v-icon>fullscreen</v-icon>
+      </v-btn>
+    </v-hover>
+    <iframe :srcDoc="srcDoc" class="previewIFrame" />
+    <v-dialog v-model="fullscreen" fullscreen hide-overlay transition="dialog-right-transition">
+      <v-card height="100%" width="100%" flat tile>
+        <v-hover>
+          <v-btn
+            slot-scope="{ hover }"
+            absolute
+            dark
+            fab
+            bottom
+            right
+            small
+            class="btnOffset"
+            :style="hover ? {opacity: '1'} : {opacity: '0.3'  }"
+            @click="fullscreen = false"
+          >
+            <v-icon>fullscreen_exit</v-icon>
+          </v-btn>
+        </v-hover>
+        <iframe :srcDoc="srcDoc" class="previewIFrame" />
+      </v-card>
+    </v-dialog>
+  </v-card>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -12,7 +51,8 @@ export default {
   },
   data() {
     return {
-      srcDoc: ''
+      srcDoc: '',
+      fullscreen: false
     }
   },
   watch: {
@@ -37,5 +77,10 @@ export default {
   border: none;
   height: inherit;
   width: inherit;
+}
+
+.btnOffset {
+  margin: 0px 24px 20px 0px;
+  bottom: 0px !important;
 }
 </style>
