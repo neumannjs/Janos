342780c5557f6e614a17fcb8d2930c3ed75014ef
Gijs van Dam <gvandam@gmail.com>
2021-03-30 23:24:57 +0800

    Process all images

--- plugins/metalsmith-responsive-images.js
+++ plugins/metalsmith-responsive-images.js
@@ -1,6 +1,11 @@
 const debug = require('debug')('plugins/metalsmith-responsive-images')
 const multimatch = require('multimatch')
-const { sniffKeyType, getSizes } = require('../../frontend-image-encode/lib2')
+const {
+  sniffKeyType,
+  getSizes,
+  builtinDecode,
+  drawableToImageData
+} = require('../../frontend-image-encode/lib2')
 
 /*
 metalsmith-responsive-images
@@ -32,77 +37,95 @@ module.exports = function (opts) {
               [Buffer.from(githubBlob.content, 'base64')],
               p2
             )
+            let imageSetHtml = '<picture>'
             try {
               const keyType = await sniffKeyType(file)
               debug('Image file found %o with keytype %s', file, keyType)
-            } catch (err) {
-              debug('Image file appears not to be resizeable. %s', file)
-              return match
-            }
-            const imageSet = await getSizes(file, opts)
-            let imageSetHtml = '<picture>'
-            for (let i = 0; i < opts.format.length; i++) {
-              let srcSet = ''
-              let sizes = ''
-              // Run through the image array from smallest to largest, so backwards
-              for (let j = imageSet[opts.format[i]].length - 1; j >= 0; j--) {
-                const image = imageSet[opts.format[i]][j]
-                // add source to the srcSet
-                srcSet +=
-                  (metalsmith._metadata.rootpath + image.name).replace(
-                    /\/\//g,
-                    '/'
-                  ) +
-                  ' ' +
-                  image.width +
-                  'w,'
-              }
-              for (let j = opts['sizes-attr'].length - 1; j >= 0; j--) {
-                if (
-                  opts['sizes-attr'].length === imageSet[opts.format[i]].length
-                ) {
-                  // add sizesAttr and replace ${width} with the actual width of the image
+              const imageSet = await getSizes(file, opts)
+              for (let i = 0; i < opts.format.length; i++) {
+                let srcSet = ''
+                let sizes = ''
+                // Run through the image array from smallest to largest, so backwards
+                for (let j = imageSet[opts.format[i]].length - 1; j >= 0; j--) {
                   const image = imageSet[opts.format[i]][j]
-                  sizes +=
-                    opts['sizes-attr'][j].replace('%width', image.width) + ','
+                  // add source to the srcSet
+                  srcSet +=
+                    (metalsmith._metadata.rootpath + image.name).replace(
+                      /\/\//g,
+                      '/'
+                    ) +
+                    ' ' +
+                    image.width +
+                    'w,'
+                }
+                for (let j = opts['sizes-attr'].length - 1; j >= 0; j--) {
+                  if (
+                    opts['sizes-attr'].length ===
+                    imageSet[opts.format[i]].length
+                  ) {
+                    // add sizesAttr and replace ${width} with the actual width of the image
+                    const image = imageSet[opts.format[i]][j]
+                    sizes +=
+                      opts['sizes-attr'][j].replace('%width', image.width) + ','
+                  } else {
+                    // cardinality sizes-attr array isn't equal to imageSet, so only add sizesAttr
+                    sizes += opts['sizes-attr'][j]
+                  }
+                }
+                srcSet = srcSet.substr(0, srcSet.length - 1)
+                sizes = sizes.substr(0, sizes.length - 1)
+                if (opts.format[i] === 'jpg') {
+                  const smallestImage = imageSet.jpg[imageSet.jpg.length - 1]
+                  const largestImage = imageSet.jpg[0]
+                  imageSetHtml += '<img srcSet="' + srcSet
+                  imageSetHtml += sizes === '' ? '' : '" sizes="' + sizes
+                  imageSetHtml +=
+                    '" src="' +
+                    (
+                      metalsmith._metadata.rootpath + smallestImage.name
+                    ).replace(/\/\//g, '/') +
+                    '" alt="' +
+                    p1 +
+                    '" title="' +
+                    p3 +
+                    '" loading="lazy" decoding="async"'
+
+                  imageSetHtml +=
+                    sizes !== ''
+                      ? ''
+                      : ' width="' +
+                        largestImage.width +
+                        '" height="' +
+                        largestImage.height +
+                        '"'
+                  imageSetHtml += '/>'
                 } else {
-                  // cardinality sizes-attr array isn't equal to imageSet, so only add sizesAttr
-                  sizes += opts['sizes-attr'][j]
+                  imageSetHtml += '<source srcSet="' + srcSet
+                  imageSetHtml += sizes === '' ? '' : '" sizes="' + sizes
+                  imageSetHtml += '" type="image/' + opts.format[i] + '"/>'
                 }
               }
-              srcSet = srcSet.substr(0, srcSet.length - 1)
-              sizes = sizes.substr(0, sizes.length - 1)
-              if (opts.format[i] === 'jpg') {
-                const smallestImage = imageSet.jpg[imageSet.jpg.length - 1]
-                const largestImage = imageSet.jpg[0]
-                imageSetHtml += '<img srcSet="' + srcSet
-                imageSetHtml += sizes === '' ? '' : '" sizes="' + sizes
-                imageSetHtml +=
-                  '" src="' +
-                  (metalsmith._metadata.rootpath + smallestImage.name).replace(
-                    /\/\//g,
-                    '/'
-                  ) +
-                  '" alt="' +
-                  p1 +
-                  '" title="' +
-                  p3 +
-                  '" loading="lazy" decoding="async"'
-
-                imageSetHtml +=
-                  sizes !== ''
-                    ? ''
-                    : ' width="' +
-                      largestImage.width +
-                      '" height="' +
-                      largestImage.height +
-                      '"'
-                imageSetHtml += '/>'
-              } else {
-                imageSetHtml += '<source srcSet="' + srcSet
-                imageSetHtml += sizes === '' ? '' : '" sizes="' + sizes
-                imageSetHtml += '" type="image/' + [opts.format[i]] + '"/>'
-              }
+            } catch (err) {
+              debug('Image file appears not to be resizeable. %s', file)
+              // Image is not resizeable but we assume it is still an image
+              // (probabably a gif) so we load it anyway and make a simple
+              // picture element
+              const result = drawableToImageData(await builtinDecode(file))
+              imageSetHtml +=
+                '<img src="' +
+                (metalsmith._metadata.rootpath + file.name).replace(
+                  /\/\//g,
+                  '/'
+                ) +
+                '" alt="' +
+                p1 +
+                '" title="' +
+                p3 +
+                '" loading="lazy" decoding="async" width="' +
+                result.width +
+                '" height="' +
+                result.height +
+                '"/>'
             }
             if (opts.caption) {
               imageSetHtml += '<figcaption>' + p3 + '</figcaption>'
